a0f572412f744c1c2e961fe311d34c66d03e2a6e
jenishbekor <57383078+jenishbekor@users.noreply.github.com>
2025-09-08 10:48:55 +0600

    first

---
+++ draw.js
@@ -0,0 +1,36 @@
+// Get canvas context
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+// Helper functions
+function Circle(x, y, r) {
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, Math.PI * 2);
+    ctx.stroke();
+}
+
+function Rectangle(x, y, w, h) {
+    ctx.strokeRect(x, y, w, h);
+}
+
+function Line(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+}
+
+function Triangle(x1, y1, x2, y2, x3, y3) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.lineTo(x3, y3);
+    ctx.closePath();
+    ctx.stroke();
+}
+
+
+Rectangle(80, 120, 80, 60)
+
+
+
